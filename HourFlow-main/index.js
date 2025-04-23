9dfb93e7c8a3183ca50f808d68bb21adb7b69a19
KrisBrabander <krisbrabander@gmail.com>
2025-04-23 20:06:28 +0200

    Delete HourFlow-main directory

--- HourFlow-main/index.js
+++
@@ -1,3 +0,0 @@
-// Dit bestand dient als entry point voor Vercel
-// Het is niet nodig voor de applicatie zelf, maar helpt Vercel om te begrijpen dat dit een statische site is
-console.log('HourFlow Premium - Statische applicatie geladen');
